b88fe6b2c19a7fe4cac89917c26088ec2c7119b6
locpe <locpeter@gmail.com>
2020-02-07 14:56:33 +0100

    Added all old files

---
+++ src/test/java/xml/UITest.java
@@ -0,0 +1,35 @@
+package xml;
+
+
+import model.Graph;
+import org.junit.Before;
+import org.junit.Test;
+import paths.Dijkstra;
+import swingui.SimpleUI;
+
+public class UITest {
+
+    Graph graph;
+    String fileName = "jelling";
+    Dijkstra dijkstra = new Dijkstra();
+
+    @Before
+    public void setUp() {
+        XMLFilter xmlFilter = new XMLFilter(fileName);
+        xmlFilter.executeFilter();
+        XMLGraphExtractor xmlGraphExtractor = new XMLGraphExtractor(fileName, xmlFilter.getValidNodes());
+        xmlGraphExtractor.executeExtractor();
+        graph = xmlGraphExtractor.getGraph();
+    }
+
+    @Test
+    public void runSimpleUI() {
+        SimpleUI simpleUI = new SimpleUI(graph);
+        simpleUI.generateUI();
+        simpleUI.setVisible(true);
+        Dijkstra.trace = false;
+        Dijkstra.result = true;
+        while (true) {}
+    }
+
+}
