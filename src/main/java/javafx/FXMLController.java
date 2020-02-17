49b59fe60ce40444e6952b0b6e033928d488c347
Benjamin Birch Hansen <bbhbenjamin@gmail.com>
2020-02-17 14:51:26 +0100

    No zoom, no move. Denmark works

--- src/main/java/javafx/FXMLController.java
+++ src/main/java/javafx/FXMLController.java
@@ -29,6 +29,11 @@ public class FXMLController implements Initializable {
     int yOffset = 7700;
     double canvasHeight;
     double canvasWidth;
+    private double mapWidthRatio;
+    private double mapHeightRatio;
+    private PixelPoint minXY;
+    private PixelPoint maxXY;
+    private double globalRatio;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -37,23 +42,94 @@ public class FXMLController implements Initializable {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        handleCanvasDrawing();
     }
 
     public void setUp() throws FileNotFoundException {
-        /*XMLFilter xmlFilter = new XMLFilter(fileName);
-        xmlFilter.executeFilter();
-        XMLGraphExtractor xmlGraphExtractor = new XMLGraphExtractor(fileName, xmlFilter.getValidNodes());
-        xmlGraphExtractor.executeExtractor();
-        graph = xmlGraphExtractor.getGraph();*/
-        PBFParser pbfParser = new PBFParser("isle-of-man-latest.osm.pbf");
+        PBFParser pbfParser = new PBFParser("denmark-latest.osm.pbf");
         pbfParser.executePBFParser();
         graph = pbfParser.getGraph();
         // System.out.println(graph.getAdjList());
+        System.out.println(canvasHeight);
         gc = canvas.getGraphicsContext2D();
         canvasHeight = canvas.getHeight();
         canvasWidth = canvas.getWidth();
+        minXY = new PixelPoint(-1, -1);
+        maxXY = new PixelPoint(-1, -1);
+        List<Node> nodeList = graph.getNodeList();
+        for (int i = 0; i < nodeList.size(); i++) {
+            Node n = nodeList.get(i);
+            double x = projectXCordMercator(n.longitude);
+            double y = projectYCordMercator(n.latitude);
+
+            minXY.x = (minXY.x == -1) ? x : Math.min(minXY.x, x);
+            minXY.y = (minXY.y == -1) ? y : Math.min(minXY.y, y);
+        }
+
+        for (int i = 0; i < nodeList.size(); i++) {
+            Node n = nodeList.get(i);
+            double x = projectXCordMercator(n.longitude) - minXY.x;
+            double y = projectYCordMercator(n.latitude) - minXY.y;
+            maxXY.x = (maxXY.x == -1) ? x : Math.max(maxXY.x, x);
+            maxXY.y = (maxXY.y == -1) ? y : Math.max(maxXY.y, y);
+        }
+        // determine the width and height ratio because we need to magnify the map to fit into the given image dimension
+        System.out.println("maxXY.x: " + maxXY.x);
+        System.out.println("maxXY.y: " + maxXY.y);
+        System.out.println("minXY.x: " + minXY.x);
+        System.out.println("minXY.y: " + minXY.y);
+        mapWidthRatio = canvasWidth / maxXY.x;
+        mapHeightRatio = canvasHeight / maxXY.y;
+        System.out.println("MapheightRatio: " + mapHeightRatio);
+        System.out.println("MapwidthRatio: " + mapWidthRatio);
+        System.out.println("Canvas height: " + canvasHeight);
+        System.out.println("Canvas width: " + canvasWidth);
+        // using different ratios for width and height will cause the map to be stretched. So, we have to determine
+        // the global ratio that will perfectly fit into the given image dimension
+        globalRatio = Math.min(mapWidthRatio, mapHeightRatio);
+        gc.setStroke(Color.VIOLET);
+        gc.setLineWidth(1.0);
+        List<List<Edge>> adjList = graph.getAdjList();
+        resetIsDrawn(adjList);
+        Gerbil();
     }
+
+    private void Gerbil() {
+        List<Node> nodeList = graph.getNodeList();
+        List<List<Edge>> adjList = graph.getAdjList();
+        for (int i = 0; i < adjList.size(); i++) {
+            Node nx = nodeList.get(i);
+            for (Edge edge : adjList.get(i)) {
+                Node ny = nodeList.get(edge.to);
+                Edge oppositeEdge = findOppositeEdge(adjList, i, edge);
+                if (oppositeEdge == null || edge.isBetter(oppositeEdge)) {
+                    double x1 = projectXCordMercator(nx.longitude) - minXY.x;
+                    double y1 = projectYCordMercator(nx.latitude) - minXY.y;
+
+                    double x2 = projectXCordMercator(ny.longitude) - minXY.x;
+                    double y2 = projectYCordMercator(ny.latitude) - minXY.y;
+
+                    double adjustedX1 = ((x1 * globalRatio));
+                    double adjustedY1 = (canvasHeight - (y1 * globalRatio));
+
+                    double adjustedX2 = ((x2 * globalRatio));
+                    double adjustedY2 = (canvasHeight - (y2 * globalRatio));
+                    /*System.out.println("---------------");
+                    System.out.println("(" + x1 + "," + y1 + ") -> (" + x2 + "," + y2 + ")");
+                    System.out.println("(" + adjustedX1 + "," + adjustedY1 + ") -> (" + adjustedX2 + "," + adjustedY2 + ")");*/
+                    gc.setStroke(Color.BLACK);
+                    if (edge.visited) {
+                        gc.setStroke(Color.BLUE);
+                    }
+                    if (edge.inPath) {
+                        gc.setStroke(Color.RED);
+                    }
+                    gc.strokeLine(adjustedX1, adjustedY1, adjustedX2, adjustedY2);
+                    edge.isDrawn = true;
+                }
+            }
+        }
+    }
+
 
     private void drawEdges() {
         gc.setStroke(Color.VIOLET);
@@ -111,6 +187,7 @@ public class FXMLController implements Initializable {
             gc.setStroke(Color.RED);
         }
         gc.strokeLine(x1, y1, x2, y2);
+        gc.strokeText("Yo", 500, 500);
         edge.isDrawn = true;
         //graphicsContext.drawString("" + Math.round(edge.d), (x1 + x2) * 0.5f, (y1 + y2) * 0.5f);
     }
@@ -183,6 +260,6 @@ public class FXMLController implements Initializable {
     }
 
     public void handleCanvasDrawing() {
-        drawEdges();
+        Gerbil();
     }
 }
---
+++ src/main/java/javafx/PixelPoint.java
@@ -0,0 +1,11 @@
+package javafx;
+
+public class PixelPoint {
+    public double x;
+    public double y;
+
+    public PixelPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
