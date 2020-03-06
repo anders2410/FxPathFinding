1f247bde289e73e48bd80ad13c7790cbe71f4501
PeterLocher <201708046@post.au.dk>
2020-03-06 15:54:56 +0100

    Reverse mercators

---
+++ src/test/java/javafx/FXMLControllerTest.java
@@ -0,0 +1,38 @@
+package javafx;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+public class FXMLControllerTest {
+
+    FXMLController fxmlController;
+    Random random;
+
+    @Before
+    public void setUp() {
+        fxmlController = new FXMLController();
+        random = new Random(3);
+    }
+
+    @Test
+    public void testMercatorX() {
+        for (int i = 0; i < 100; i++) {
+            double cord = random.nextDouble();
+            double cordCopy = fxmlController.reverseMercatorX(fxmlController.mercatorX(cord));
+            assertEquals(cordCopy, cord, 0.000000000000001);
+        }
+    }
+
+    @Test
+    public void testMercatorY() {
+        for (int i = 0; i < 100; i++) {
+            double cord = random.nextDouble();
+            double cordCopy = fxmlController.reverseMercatorY(fxmlController.mercatorY(cord));
+            assertEquals(cordCopy, cord, 0.00000000000001);
+        }
+    }
+}
