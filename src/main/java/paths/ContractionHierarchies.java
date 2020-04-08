b3d14cc1aa44a3aeee5a6291762e185e6d4059bf
Anders Holt Knudsen <andhk11@gmail.com>
2020-04-08 14:51:30 +0200

    Added comments in ContractionHierarchies

--- src/main/java/paths/ContractionHierarchies.java
+++ src/main/java/paths/ContractionHierarchies.java
@@ -1,6 +1,9 @@
 package paths;
 
 import model.Graph;
+import model.Node;
+
+import java.util.PriorityQueue;
 
 /**
  * This class implements the algorithm behind Contraction Hierarchies. Both doing the augmented graph
@@ -8,27 +11,57 @@ import model.Graph;
  */
 public class ContractionHierarchies {
     private Graph graph;
+    private Graph augmentedGraph;
+    private PriorityQueue<Node> queue;
+
+    public ContractionHierarchies(Graph graph) {
+        this.graph = graph;
+        this.queue = new PriorityQueue<>();
+    }
 
     /**
-     * The edgeDifference is defined by edgeDifference = s(v) - in(v) - out(v). Where s(v) is the number of
-     * added shortcuts, in(v) is incoming degree and out(v) is outgoing degree.
-     *
-     * @return the edgeDifferece
+     * The edgeDifference is defined by edgeDifference = s(n) - in(n) - out(n). Where s(n) is the number of
+     * added shortcuts, in(n) is incoming degree and out(n) is outgoing degree. We want to contract nodes
+     * with a small edgeDifference.
      */
-    private int edgeDifference() {
+    private int edgeDifference(Node n) {
         return 0;
     }
 
-    private int contractedNeighbours() {
+    /**
+     * We are interested in spreading out the contracted nodes across the graoh so they
+     * do not cluster together. We will contract nodes with a small number of already contracted
+     * neighbours.
+     */
+    private int contractedNeighbours(Node n) {
         return 0;
     }
 
-    private int shortcutCover() {
+    /**
+     * The idea is that we want to contract important nodes later. The shortcutCover sc(n) is the
+     * number og neighbours m of n such that we have shortcut to or from m after contracting n.
+     * If the sc(n) is big, many nodes depend on n. We will contract a node with small sc(n).
+     */
+    private int shortcutCover(Node n) {
         return 0;
     }
 
-    private int nodeLevel() {
+    /**
+     * The Node Level L(n) is an upper bound on the number of edges in the shortest path from any
+     * s to n in the augmented path. Initially, L(n) is 0. After contracting node n, for neighbours u
+     * of n do L(u) <- max(L(u), L(n) + 1). We contract a node with small L(n).
+     */
+    private int nodeLevel(Node n) {
         return 0;
+    }
+
+    /**
+     * The importance is used when constructing the augmented graph. We want to contract all the
+     * nodes with low importance first, and then gradually work our way through the graph using
+     * a priorityQueue based on the importance.
+     */
+    private int getImportance(Node n) {
+        return edgeDifference(n) + contractedNeighbours(n) + shortcutCover(n) + nodeLevel(n);
     }
 
     public void setGraph(Graph graph) {
