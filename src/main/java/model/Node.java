69c92345b6a766052760cbe3b91b9556628179ce
Anders Holt Knudsen <andhk11@gmail.com>
2020-04-14 15:41:08 +0200

    Started working on ContractionHierarchies v2

--- src/main/java/model/Node.java
+++ src/main/java/model/Node.java
@@ -7,50 +7,16 @@ public class Node implements Serializable {
     public double longitude;
     public double latitude;
 
-    private boolean contracted;
-    private int importance;
-    private int contractedNeighbours;
-
     public Node(int index, double longitude, double latitude) {
         this.index = index;
         this.latitude = latitude;
         this.longitude = longitude;
-        contracted = false;
-        importance = 0;
-        contractedNeighbours = 0;
     }
 
     public Node(Node node) {
         this.index = node.index;
         this.latitude = node.latitude;
         this.longitude = node.longitude;
-        contracted = false;
-        importance = 0;
-        contractedNeighbours = 0;
-    }
-
-    public boolean isContracted() {
-        return contracted;
-    }
-
-    public void setContracted(boolean contracted) {
-        this.contracted = contracted;
-    }
-
-    public int getImportance() {
-        return importance;
-    }
-
-    public void setImportance(int importance) {
-        this.importance = importance;
-    }
-
-    public int getContractedNeighbours() {
-        return contractedNeighbours;
-    }
-
-    public void setContractedNeighbours(int contractedNeighbours) {
-        this.contractedNeighbours = contractedNeighbours;
     }
 
     @Override
