c96d56bccce00443167c16483f751e5fe13900ed
PeterLocher <201708046@post.au.dk>
2020-04-29 15:40:04 +0200

    Test update

--- src/test/java/paths/XMLPathsTest.java
+++ src/test/java/paths/XMLPathsTest.java
@@ -8,7 +8,7 @@ import load.xml.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class SSSPTest {
+public class XMLPathsTest {
 
     Graph graph;
     String fileName = "jelling";
